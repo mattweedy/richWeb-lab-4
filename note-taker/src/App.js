6754be908fb441289297c04fb5aba52e7198091c
mattweedy <matthewtweedy02@gmail.com>
2023-12-12 19:21:41 +0000

    displaying info in card and abstracted all to one weather component

--- note-taker/src/App.js
+++ note-taker/src/App.js
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from 'react';
-import './App.css';
+import './stlyes/App.css';
 import NoteList from './components/NoteList';
 import NoteForm from './components/NoteForm';
+import Weather from './components/Weather';
 
 // TODO: extra functions:
 //       - swap css file to darkmode depending on time
@@ -10,13 +11,13 @@ import NoteForm from './components/NoteForm';
 const App = () => {
     const [notes, setNotes] = useState([]);
     const [currentNote, setCurrentNote] = useState(null);
-    const [lat, setLat] = useState([]);
-    const [long, setLong] = useState([]);
+
     const[weatherData, setWeatherData] = useState({});
-    const [city, setCity] = useState("");
+    const[city, setCity] = useState("");
 
     const addNote = (noteText, noteColour) => {
         if (noteText.trim() !== "") {
+            // give each note unique id with datetime
             const newNote = { id: Date.now(), text: noteText, color: noteColour };
             setNotes([...notes, newNote]);
         }
@@ -46,42 +47,19 @@ const App = () => {
         setCurrentNote(note);
     }
 
-    useEffect(() => {
-        navigator.geolocation.getCurrentPosition(function(position) {
-            setLat(position.coords.latitude);
-            setLong(position.coords.longitude);
-        });
-
-        console.log("Latitude is :", lat)
-        console.log("Longitude is :", long)
-
-        if (city) {
-            const fetchData = async () => {
-                const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=b2514be7b456d0f78a7485c408ec07e9`);
-                const data = await response.json();
-                setWeatherData(data);
-            };
-            fetchData();
-        }
-        
-    }, [lat, long, city]);
-
     return (
         <div className="grid">
             <div id="notes">
-                <h2 id="stored-notes-title">Stored notes</h2>
+                <h1 id="stored-notes-title">Stored notes</h1>
                 <NoteList notes={notes} selectNote={selectNote} />
             </div>
 
             <div id="notes-new">
                 <NoteForm addNote={addNote} editNote={editNote} deleteNote={deleteNote} currentNote={currentNote} setCurrentNote={setCurrentNote} />
-            <div id="weather">
-                <input type="text" value={city} onChange={(e) => setCity(e.target.value)} />
-                {weatherData.name && <h2>{weatherData.name}</h2>}
-                {weatherData.main && <h3>{Math.round(weatherData.main.temp - 273.15)}°C</h3>}
-                {weatherData.main && <p>Feels like: {Math.round(weatherData.main.feels_like - 273.15)}°C</p>}
-                {weatherData.weather && <p>{weatherData.weather[0].description}</p>}
-            </div>
+
+                <br></br>
+
+                <Weather></Weather>
             </div>
 
         </div>
---
+++ note-taker/src/components/Weather.js
@@ -0,0 +1,47 @@
+import React, { useEffect, useState } from "react";
+import { Card } from 'semantic-ui-react';
+import moment from 'moment'
+import '../stlyes/WeatherCard.css';
+
+const WeatherCard = () => {
+    const [city, setCity] = useState("");
+    const [weatherData, setWeatherData] = useState({});
+
+    useEffect(() => {
+        if (city) {
+          const fetchData = async () => {
+            const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=8c0597e030b36e3a2aeb27ce07b090d0`);
+            const data = await response.json();
+            setWeatherData(data);
+          };
+          fetchData();
+        }
+      }, [city]);
+
+      return (
+        <div>
+            <h2>Search City for Weather</h2>
+            <input type="text" value={city} onChange={(e) => setCity(e.target.value)} style={{width: '100%', height: '30px', fontSize: '16px', backgroundColor: '#f0f0f0', color: '#333', border: '1px solid #ccc'}} />
+
+
+            {(typeof weatherData.main != 'undefined') ? (
+                <Card className="horizontal-card">
+                    <Card.Content>
+                        <Card.Header className="header">{weatherData.name}</Card.Header>
+                        <br></br>
+                        <p>Day: {moment().format('dddd')}</p>
+                        <p>Date: {moment().format('LL')}</p>
+                        <p>Temperature: {Math.round(weatherData.main.temp - 273.15)}°C</p>
+                        {weatherData.main && <p>Feels like: {Math.round(weatherData.main.feels_like - 273.15)}°C</p>}
+                        <p>Description: {weatherData.weather[0].description}</p>
+                        <p>Humidity: {weatherData.main.humidity} %</p>
+                    </Card.Content>
+                </Card>
+            ): (
+                <div></div>
+            )}
+        </div>
+    );
+}
+
+export default WeatherCard;
