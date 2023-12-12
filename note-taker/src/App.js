2d3cdf01c7119fcf5859758fe18a93c45d203ad2
mattweedy <matthewtweedy02@gmail.com>
2023-12-12 17:51:36 +0000

    successfully displaying weather api data on app

--- note-taker/src/App.js
+++ note-taker/src/App.js
@@ -1,12 +1,19 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import NoteList from './components/NoteList';
 import NoteForm from './components/NoteForm';
 
-// app component
+// TODO: extra functions:
+//       - swap css file to darkmode depending on time
+//       - something else
+
 const App = () => {
     const [notes, setNotes] = useState([]);
     const [currentNote, setCurrentNote] = useState(null);
+    const [lat, setLat] = useState([]);
+    const [long, setLong] = useState([]);
+    const[weatherData, setWeatherData] = useState({});
+    const [city, setCity] = useState("");
 
     const addNote = (noteText, noteColour) => {
         if (noteText.trim() !== "") {
@@ -39,6 +46,26 @@ const App = () => {
         setCurrentNote(note);
     }
 
+    useEffect(() => {
+        navigator.geolocation.getCurrentPosition(function(position) {
+            setLat(position.coords.latitude);
+            setLong(position.coords.longitude);
+        });
+
+        console.log("Latitude is :", lat)
+        console.log("Longitude is :", long)
+
+        if (city) {
+            const fetchData = async () => {
+                const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=b2514be7b456d0f78a7485c408ec07e9`);
+                const data = await response.json();
+                setWeatherData(data);
+            };
+            fetchData();
+        }
+        
+    }, [lat, long, city]);
+
     return (
         <div className="grid">
             <div id="notes">
@@ -48,7 +75,15 @@ const App = () => {
 
             <div id="notes-new">
                 <NoteForm addNote={addNote} editNote={editNote} deleteNote={deleteNote} currentNote={currentNote} setCurrentNote={setCurrentNote} />
+            <div id="weather">
+                <input type="text" value={city} onChange={(e) => setCity(e.target.value)} />
+                {weatherData.name && <h2>{weatherData.name}</h2>}
+                {weatherData.main && <h3>{Math.round(weatherData.main.temp - 273.15)}°C</h3>}
+                {weatherData.main && <p>Feels like: {Math.round(weatherData.main.feels_like - 273.15)}°C</p>}
+                {weatherData.weather && <p>{weatherData.weather[0].description}</p>}
             </div>
+            </div>
+
         </div>
     );
 }
